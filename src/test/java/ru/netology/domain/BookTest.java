5ef777e90e14c7a2333f11c7d40424f494781727
alexkochutov <alex.kochutov@gmail.com>
2022-02-17 14:59:31 +0300

    Rewrited tests for matches method

--- src/test/java/ru/netology/domain/BookTest.java
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -5,54 +5,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
-    Product item1 = new Product(1, "t-shirt", 1200);
-    Product item2 = new Product(2, "Cover book", 200);
-    Product item3 = new Product(3, "Cover book", 300);
-    Product item4 = new Book(4, "book 1", 2700, "O'Reilly");
-    Product item5 = new Book(5, "book 2", 4900, "Oracle");
-    Product item6 = new Book(6, "journal", 850, "O'Reilly");
-    Product[] dataStorage = {item1, item2, item3, item4, item5, item6};
-    Product[] actual = new Product[0];
+    Product item1 = new Product(1, "Cover book", 1200);
+    Product item2 = new Book(2, "book 1", 2700, "O'Reilly");
 
     @Test
-    void ShouldReturnEmptyResult() {
-        for (Product product : dataStorage) {
-            if (product.matches("keyword")) {
-                Product[] tempStorage = new Product[actual.length + 1];
-                System.arraycopy(actual, 0, tempStorage, 0, actual.length);
-                tempStorage[tempStorage.length - 1] = product;
-                actual = tempStorage;
-            }
-        }
-        Product[] expected = {};
-        assertArrayEquals(expected, actual);
+    void shouldReturnFalseWithEmptyKeyword() {
+        assertFalse(item1.matches("''"));
     }
 
     @Test
-    void shouldReturnResultFromSuper() {
-        for (Product product : dataStorage) {
-            if (product.matches("book")) {
-                Product[] tempStorage = new Product[actual.length + 1];
-                System.arraycopy(actual, 0, tempStorage, 0, actual.length);
-                tempStorage[tempStorage.length - 1] = product;
-                actual = tempStorage;
-            }
-        }
-        Product[] expected = {item2, item3, item4, item5};
-        assertArrayEquals(expected, actual);
+    void shouldReturnFalseWithWrongKeyword() {
+        assertFalse(item1.matches("phone"));
     }
 
     @Test
-    void shouldReturnResultFromThis() {
-        for (Product product : dataStorage) {
-            if (product.matches("O'Reilly")) {
-                Product[] tempStorage = new Product[actual.length + 1];
-                System.arraycopy(actual, 0, tempStorage, 0, actual.length);
-                tempStorage[tempStorage.length - 1] = product;
-                actual = tempStorage;
-            }
-        }
-        Product[] expected = {item4, item6};
-        assertArrayEquals(expected, actual);
+    void shouldReturnTrueFromSuper() {
+        assertTrue(item2.matches("book"));
+    }
+
+    @Test
+    void shouldReturnTrueFromThis() {
+        assertTrue(item2.matches("O'Reilly"));
     }
 }
--- src/test/java/ru/netology/domain/SmartphoneTest.java
+++ src/test/java/ru/netology/domain/SmartphoneTest.java
@@ -6,53 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SmartphoneTest {
     Product item1 = new Product(1, "t-shirt", 1200);
-    Product item2 = new Product(2, "phone case", 1500);
-    Product item3 = new Product(3, "phone glass", 1000);
-    Product item4 = new Smartphone(4, "smartphone 1", 120000, "apple");
-    Product item5 = new Smartphone(5, "smartphone 2", 55000, "xiaomi");
-    Product item6 = new Smartphone(6, "smart watch", 25000, "xiaomi");
-    Product[] dataStorage = {item1, item2, item3, item4, item5, item6};
-    Product[] actual = new Product[0];
+    Product item2 = new Smartphone(2, "smartphone 1", 120000, "apple");
+
 
     @Test
-    void ShouldReturnEmptyResult() {
-        for (Product product : dataStorage) {
-            if (product.matches("keyword")) {
-                Product[] tempStorage = new Product[actual.length + 1];
-                System.arraycopy(actual, 0, tempStorage, 0, actual.length);
-                tempStorage[tempStorage.length - 1] = product;
-                actual = tempStorage;
-            }
-        }
-        Product[] expected = {};
-        assertArrayEquals(expected, actual);
+    void shouldReturnFalseWithEmptyKeyword() {
+        assertFalse(item1.matches("''"));
     }
 
     @Test
-    void shouldReturnResultFromSuper() {
-        for (Product product : dataStorage) {
-            if (product.matches("phone")) {
-                Product[] tempStorage = new Product[actual.length + 1];
-                System.arraycopy(actual, 0, tempStorage, 0, actual.length);
-                tempStorage[tempStorage.length - 1] = product;
-                actual = tempStorage;
-            }
-        }
-        Product[] expected = {item2, item3, item4, item5};
-        assertArrayEquals(expected, actual);
+    void shouldReturnFalseWithWrongKeyword() {
+        assertFalse(item1.matches("book"));
     }
 
     @Test
-    void shouldReturnResultFromThis() {
-        for (Product product : dataStorage) {
-            if (product.matches("xiaomi")) {
-                Product[] tempStorage = new Product[actual.length + 1];
-                System.arraycopy(actual, 0, tempStorage, 0, actual.length);
-                tempStorage[tempStorage.length - 1] = product;
-                actual = tempStorage;
-            }
-        }
-        Product[] expected = {item5, item6};
-        assertArrayEquals(expected, actual);
+    void shouldReturnTrueFromSuper() {
+        assertTrue(item2.matches("phone"));
+    }
+
+    @Test
+    void shouldReturnTrueFromThis() {
+        assertTrue(item2.matches("apple"));
     }
 }
